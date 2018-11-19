2f18bf2ad10f9975b691f442b085cd6c495b3d1d
司 金子 <160049@jc-21.jp>
2018-11-19 12:48:11 +0900

    プロジェクト ファイルを追加します。

---
+++ BMI/BMI/BmiResult.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BmiResult : ContentPage
+	{
+        
+		public BmiResult ()
+		{
+			InitializeComponent ();
+            buttonBack.Clicked += buttonBackClicked;
+		}
+        public BmiResult(float height,int weight)
+        {
+            InitializeComponent();
+            
+            labelBMI.Text = "" + weight / (height * height);
+            buttonBack.Clicked += buttonBackClicked;
+        }
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+	}
+}
---
+++ BMI/BMI/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ButtonBMI.Clicked += ButtonBmiClickEvent;
+            ButtonBMI2.Clicked += ButtonClickEvent2;
+        }
+
+        private void ButtonClickEvent2(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            float height = float.Parse(EntryHeight.Text);
+            int weight = int.Parse(EntryWeight.Text);
+            Navigation.PushModalAsync(new NavigationPage(new BmiResult
+                (height,weight)));
+        }
+
+        private void ButtonBmiClickEvent(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            String a = EntryHeight.Text;
+            String b = EntryWeight.Text;
+            float height = float.Parse(a);
+            int weight = int.Parse(b);
+
+            LabelResult.Text = ""+weight/ (height * height);
+            
+        }
+    }
+}
